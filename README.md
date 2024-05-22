81daf85072f351f29a1224469cb0e81dd8f03b30
Gebolze <gebolze@gmail.com>
2024-05-22 11:20:01 +0200

    Added disko configuration

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # nixos-config
+
+Configuration for my personal nixOS setup.
+
+## Partition layout
+
+- `/dev/nvme1n1p1` -- /boot 512MB, FAT32
+- `/dev/nvme1n1p2` -- the rest, btrfs
+  - `@` -- `/` mount options: `compress=zstd`
+  - `@home` -- `/home` mount options: `compress=zstd`
+  - `@nix` -- `/nix` mount options: `compress=zstd, noatime`
+  - `@swap` -- `/.swap`
+    - swapfile 32GB
+
+The layout above is declaratively configured in `disk-config.nix` file.
+
+**Before you apply the configuration review the file and update the device name
+accordingly.**
+
+To apply the configuration the follow command can be used:
+```sh
+$ sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- \
+  --mode disko /disk-config.nix
+```
---
+++ disk-config.nix
@@ -0,0 +1,58 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme1n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # override existing partition
+
+                subvolumes = {
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" ];
+                  };
+
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" ];
+                  };
+
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+
+                  "@swap" = {
+                    mountpoint = "/.swap";
+                    swap = {
+                      swapfile = {
+                        size = "32G";
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
