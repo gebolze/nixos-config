5b452879f5631237f51d88789b81dfbfe21e5441
Markus Burkert <gebolze@gmail.com>
2024-07-02 11:10:40 +0200

    enabled flakes and nix-command

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,9 @@
     ];
   };
 
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   # environment.systemPackages = with pkgs; [
