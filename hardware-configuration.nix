254e0df4d1e342ac53207d9009b3918f9eee5e63
Markus Burkert <gebolze@gmail.com>
2024-07-02 09:40:26 +0200

    added missing swapfile

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -43,7 +43,7 @@
       options = [ "subvol=@nix" "compress=zstd" "noatime" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [ { device = "/.swap/swapfile"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
