4d5ea35fbd77cb156e9f2876c44bedb4a562c506
Gebolze <gebolze@gmail.com>
2024-07-02 07:26:43 +0200

    Fixed syntax error on esp partition size

--- disk-config.nix
+++ disk-config.nix
@@ -9,7 +9,7 @@
           partitions = {
             ESP = {
               type = "EF00";
-              size "512M";
+              size = "512M";
               content = {
                 type = "filesystem";
                 format = "vfat";
