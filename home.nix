e303e2b6c15e45dbe592417b5ad7c5e16bc53e92
Markus Burkert <gebolze@gmail.com>
2024-07-03 15:28:09 +0200

    Configured git user name & email depending on git directory

--- home.nix
+++ home.nix
@@ -12,6 +12,36 @@
     tree
   ];
 
+
+  programs.git = {
+    enable = true;
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+    };
+    includes = [
+      {
+        contents = {
+          user = {
+            name = "Gebolze";
+            email = "gebolze@gmail.com";
+          };
+        };
+      }
+
+      {
+        condition = "gitdir:~/code/work/";
+        contents = {
+          user = {
+            name = "Markus Burkert";
+            email = "m.burkert@cid.com";
+          };
+        };
+      }
+    ];
+  };
+
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new home Manager release introduces backwards
