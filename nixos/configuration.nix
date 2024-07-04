1366d31b47d8e476a99b5c2d6ef658567b9979c7
Gebolze <gebolze@gmail.com>
2024-07-04 15:31:00 +0200

    configured locales to use german for LC_TIME and LC_MONETARY

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./locale.nix
       ./pipewire.nix
     ];
 
@@ -45,12 +46,9 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "de-latin1-nodeadkeys";
-  #   useXkbConfig = true; # use xkb.options in tty.
   };
 
   # Configuring the NIVIDIA driver
---
+++ nixos/locale.nix
@@ -0,0 +1,10 @@
+{
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+    };
+    supportedLocales = [ "C.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" "de_DE.UTF-8/UTF-8" ];
+  };
+}
