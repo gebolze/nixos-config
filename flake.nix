c9a370e3ae1a9c154fd28e746a7211dca06042a7
Gebolze <gebolze@gmail.com>
2024-07-07 11:02:13 +0200

    Added initial qemu guest configuration

--- flake.nix
+++ flake.nix
@@ -27,6 +27,15 @@
           ./nixos/hosts/ryzen/configuration.nix
         ];
       };
+
+      testing = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./nixos/hosts/testing/disko-config.nix { _module.args.disks = [ "/dev/vda" ]; }
+          ./nixos/hosts/testing/configuration.nix
+        ];
+      };
     };
     
     homeConfigurations = {
---
+++ nixos/hosts/testing/disko-config.nix
@@ -0,0 +1,58 @@
+{disks ? [ "/dev/vda" ], ... }: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "1G";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # override existing partition
+
+                subvolumes = {
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" ];
+                  };
+
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" ];
+                  };
+
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+
+                  "@swap" = {
+                    mountpoint = "/.swap";
+                    swap = {
+                      swapfile = {
+                        size = "6G";
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
