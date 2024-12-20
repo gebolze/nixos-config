284b9c78c366f767062aa184fa1ff7bd7b4e8976
Gebolze <gebolze@gmail.com>
2024-12-20 07:07:31 +0100

    Added comment to pipewire module

--- nixos/pipewire.nix
+++ nixos/pipewire.nix
@@ -1,11 +1,12 @@
 {
+  # enable RealtimeKit to allow pipewire to acquire realtime priority
   security.rtkit.enable = true;
-  hardware.pulseaudio.enable = false;
+
+  # enable pipewire with Alsa and PulseAudio support
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
   };
 }
